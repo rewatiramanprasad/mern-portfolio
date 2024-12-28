eb9c3e180a102e61861219ab4e99cf31c8d31c92
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-28 14:40:07 +0530

    feat: created navbar

---
+++ src/NavBar.jsx
@@ -0,0 +1,20 @@
+import { links } from './data'
+export default function NavBar() {
+  return (
+    <nav className="bg-emerald-100 px-6 py-8  flex flex-row gap-x-12 sm:flex-col sm:gap-y-5">
+      <h2 className="text-3xl font-bold">
+        Web<span className="text-emerald-700">Dev</span>
+      </h2>
+      <div className='text-xl flex gap-x-4'>
+        {links.map((link) => {
+          const { id, href, label } = link
+          return (
+            <a href={href} key={id}>
+              {label}
+            </a>
+          )
+        })}
+      </div>
+    </nav>
+  )
+}

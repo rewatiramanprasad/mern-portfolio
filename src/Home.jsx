12551e8f17f14f045bc2d14a54bf4823a9f86be0
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-28 14:40:25 +0530

    feat: created hero

---
+++ src/Home.jsx
@@ -0,0 +1,32 @@
+import { FaGithubSquare, FaGitSquare, FaLinkedin, FaTwitter, FaTwitterSquare } from 'react-icons/fa'
+
+function Home() {
+  return (
+    <section className="  bg-emerald-100 py-24">
+      <div className=" grid md:grid-cols-2 px-8 mx-auto max-w-7xl">
+        <article>
+          <h1 className="text-7xl font-bold tracking-wider">I'm Raman</h1>
+          <p className=" mt-4 text-3xl text-slate-700 capitalize tracking-wider ">
+            Full Stack Developer
+          </p>
+          <p className=" mt-2 text-lg text-slate-700 tracking-wider">
+            Turning Ideas into Interactive Reality
+          </p>
+          <div className="flex gap-x-4 mt-4">
+            <a href="">
+              <FaGithubSquare className="h-8 w-8 text-slate-700  hover:text-black duration-300" />
+            </a>
+            <a href="https://www.linkedin.com/in/rewatiramanprasad/">
+              <FaLinkedin className="h-8 w-8 text-slate-700  hover:text-black duration-300 " />
+            </a>
+            <a href="">
+              <FaTwitterSquare className="h-8 w-8 text-slate-700  hover:text-black duration-300 " />
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  )
+}
+
+export default Home

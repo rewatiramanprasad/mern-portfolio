24d5fa88282ae27635748dd007501d32c7e14f2b
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-28 14:39:31 +0530

    feat: added links

---
+++ src/data.jsx
@@ -0,0 +1,8 @@
+import { nanoid } from "nanoid";
+
+export const links = [
+  { id: nanoid(), href: '#Home', label: 'Home' },
+  { id: nanoid(), href: '#About', label: 'About' },
+  { id: nanoid(), href: '#Skills', label: 'Skills' },
+  { id: nanoid(), href: '#Projects', label: 'Projects' },
+]

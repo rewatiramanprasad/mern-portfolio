31819d2012bb8087536ae595268b52820caa5b8a
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-29 20:40:16 +0530

    fix: id

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ import SectionTitle from './SectionTitle'
 import about from './assets/about.svg'
 function About() {
   return (
-    <section className="py-20 bg-white " id="about">
+    <section className="py-20 bg-white " id="About">
       <div className="align-element grid md:grid-cols-2 items-center gap-16">
         <img src={about} alt="about" className="h-64 w-full " />
         <article>

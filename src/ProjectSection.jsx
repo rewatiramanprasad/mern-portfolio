a045dc83660391f29bc339ab0e7e3c68c48d45fe
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-29 20:27:33 +0530

    feat: added project section

---
+++ src/ProjectSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import SectionTitle from './SectionTitle'
+import { projects } from './data'
+import ProjectCard from './ProjectCard'
+
+function ProjectSection() {
+  return (
+    <section className="py-20 align-element" id="projects">
+      <SectionTitle title={'Web Creations'} />
+      <div className=" grid py-16 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+        {projects.map((project) => {
+          const { id } = project
+          return <ProjectCard key={id} {...project} />
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default ProjectSection

b8142cc270bc7f6bce3ffd54de1e83631380fc3d
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-29 20:29:28 +0530

    refactor: id

--- src/ProjectSection.jsx
+++ src/ProjectSection.jsx
@@ -5,7 +5,7 @@ import ProjectCard from './ProjectCard'
 
 function ProjectSection() {
   return (
-    <section className="py-20 align-element" id="projects">
+    <section className="py-20 align-element" id="Projects">
       <SectionTitle title={'Web Creations'} />
       <div className=" grid py-16 lg:grid-cols-2 xl:grid-cols-3 gap-8">
         {projects.map((project) => {

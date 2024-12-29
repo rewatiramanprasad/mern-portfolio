519a093bfeab084d5bb1ec0ff93bc213f22301cb
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-29 20:40:57 +0530

    style: fix in large screen

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,19 +1,21 @@
 import { links } from './data'
 export default function NavBar() {
   return (
-    <nav className="bg-emerald-100 px-6 align-element  flex flex-row gap-x-12 sm:flex-col sm:gap-y-5">
-      <h2 className="text-3xl font-bold">
-        Web<span className="text-emerald-700">Dev</span>
-      </h2>
-      <div className='text-xl flex gap-x-4'>
-        {links.map((link) => {
-          const { id, href, label } = link
-          return (
-            <a href={href} key={id}>
-              {label}
-            </a>
-          )
-        })}
+    <nav className="bg-emerald-100">
+      <div className='align-element  flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
+        <h2 className="text-3xl font-bold">
+          Web<span className="text-emerald-600">Dev</span>
+        </h2>
+        <div className="text-xl flex gap-x-3">
+          {links.map((link) => {
+            const { id, href, label } = link;
+            return (
+              <a href={href} key={id}>
+                {label}
+              </a>
+            )
+          })}
+        </div>
       </div>
     </nav>
   )

c58f6326090974c5beddc84fa44c2583507b0bfd
Raman Prasad <ramanprasad.0203@gmail.com>
2025-03-22 02:05:38 +0530

    fix: skill height

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -7,7 +7,7 @@ function Skills() {
   return (
     <section className="align-element py-20 " id="Skills">
       <SectionTitle title={'Tech Stack'} />
-      <div className=" flex lg:flex-row flex-col gap-8 overflow-hidden group h-lvh  ">
+      <div className=" flex lg:flex-row flex-col gap-8 overflow-hidden group h-lvh lg:h-full ">
         <div className="py-16 flex lg:flex-row flex-col gap-8 animate-loop-scroll-vertical lg:animate-loop-scroll-horizontal group-hover:paused">
           {skills.map((skill) => {
             const { id, title, icon, text } = skill

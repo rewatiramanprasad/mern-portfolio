20487eda5e1d17ffbe85e4b0ae48aac7df0e4bdf
Raman Prasad <ramanprasad.0203@gmail.com>
2025-03-22 23:42:40 +0530

    fix: lint error

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,9 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      ...react.configs.flat['jsx-runtime'],
+      ...react.configs.flat.recommended,
+
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
